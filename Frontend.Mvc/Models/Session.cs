44eea7d299d5b5373a786684b0c52aac73a0dd6c
Roel van de Grint <roel.van-de-grint@capgemini.com>
2021-12-12 12:26:31 +0100

    Fixed nullable warningsn

--- Frontend.Mvc/Models/Session.cs
+++ Frontend.Mvc/Models/Session.cs
@@ -2,9 +2,9 @@ namespace Frontend.Mvc.Models;
 
 public record Session
 {
-    public string Title { get; set; }
+    public string? Title { get; set; }
 
-    public string Duration { get; set; }
+    public string? Duration { get; set; }
 
-    public string Speaker { get; set; }
+    public string? Speaker { get; set; }
 }
